d16e2d60b53e57bc38356cc9c966227917833fff
San Kang <devrivmt@gmail.com>
2023-08-03 21:18:22 +0900

    Add: Popup page

---
+++ src/pages/popup.js
@@ -0,0 +1,17 @@
+// On page start
+window.onload = function() {
+    // Edit version
+    document.getElementById("version").innerText = chrome.runtime.getManifest().version
+    // Apply translation
+    applyTranslation()
+}
+
+/**
+ * Apply translations to element which has 'translate' class
+ */
+function applyTranslation() {
+    const list = document.body.getElementsByClassName('translate')
+    for(let i=0; i < list.length; i++) {
+        list[i].innerText = chrome.i18n.getMessage(list[i].getAttribute('key'))
+    }
+}
